134c79393a227bc8be4d2cc3865f1439429fa965
Daniel Martinez-Gatell <demartinezgatell@gmail.com>
2021-03-08 20:20:04 +0100

    inquirer setup

--- index.js
+++ index.js
@@ -1,4 +1,24 @@
 const chalk = require("chalk");
 const clear = require("clear");
 const figlet = require("figlet");
+
 const files = require("./lib/files");
+const inquirer = require("./lib/inquirer");
+
+clear();
+
+console.log(
+	chalk.cyan(figlet.textSync("Aivogghiee", { horizontalLayout: "full" }))
+);
+
+// if (files.directoryExists(".git")) {
+// 	console.log(chalk.red("Already a Git repository!"));
+// 	process.exit();
+// }
+
+const run = async () => {
+	const credentials = await inquirer.askGithubCredentials();
+	console.log(credentials);
+};
+
+run();
